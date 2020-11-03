a0ff4d2e01805e0c059b83565d2903126d6d09a0
Gabriel Mesquida-Masana <gabriel.mesquida.e@thalesdigital.io>
2020-11-03 15:31:15 +1100

    Different colours

--- src/IPPrintColored2020.hs
+++ src/IPPrintColored2020.hs
@@ -16,16 +16,16 @@ import           Text.Show.Pretty                    (ppShow)
 myColourPrefs :: ColourPrefs
 myColourPrefs =
   HsColour.defaultColourPrefs
-    { HsColour.conid = [HsColour.Foreground HsColour.Yellow, HsColour.Bold],
+    { HsColour.conid = [HsColour.Foreground HsColour.Blue, HsColour.Bold],
       HsColour.conop = [HsColour.Foreground HsColour.Yellow],
       HsColour.varid = [HsColour.Foreground HsColour.Cyan, HsColour.Bold],
       HsColour.varop = [HsColour.Foreground HsColour.Cyan],
-      HsColour.string = [HsColour.Foreground HsColour.Red, HsColour.Bold],
+      HsColour.string = [HsColour.Foreground (HsColour.Rgb 245 114 66), HsColour.Bold],
       HsColour.char = [HsColour.Foreground HsColour.Magenta, HsColour.Bold],
       HsColour.number = [HsColour.Foreground HsColour.Green, HsColour.Bold],
-      HsColour.layout = [HsColour.Foreground HsColour.Blue],
-      HsColour.keyglyph = [HsColour.Foreground HsColour.Magenta],
-      HsColour.comment = [HsColour.Foreground HsColour.Blue, HsColour.Bold],
+      HsColour.layout = [HsColour.Foreground (HsColour.Rgb 245 218 66), HsColour.Bold],
+      HsColour.keyglyph = [HsColour.Foreground (HsColour.Rgb 245 151 66), HsColour.Bold],
+      HsColour.comment = [HsColour.Foreground HsColour.Red, HsColour.Bold],
       HsColour.cpp = [HsColour.Foreground HsColour.Green],
       HsColour.definition = [HsColour.Foreground HsColour.Blue, HsColour.Bold]
     }
